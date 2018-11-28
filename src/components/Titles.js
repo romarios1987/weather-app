0cc75c3e21c8f31123c73711d9542319c4246ca2
romarios1987 <romios047@gmail.com>
2018-11-28 23:00:50 +0200

    creating first three components

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export default class Titles extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Weather Finder</h1>
+                <p>Find out temperature, condition and more...</p>
+            </div>
+        );
+    }
+}
+
---
+++ src/components/Weather.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Weather extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Weather@@@</h1>
+            </div>
+        );
+    }
+}
+
