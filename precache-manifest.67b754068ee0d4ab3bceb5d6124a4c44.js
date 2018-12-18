20948a6ebed70c901391cfff3a3b2d7c760a033c
romarios1987 <romios047@gmail.com>
2018-12-18 23:43:41 +0200

    Updates

---
+++ precache-manifest.67b754068ee0d4ab3bceb5d6124a4c44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "f47ba623fa3d61a3fafb",
+    "url": "/weather-app/static/js/main.f47ba623.chunk.js"
+  },
+  {
+    "revision": "4e1b29205e28e19b4da9",
+    "url": "/weather-app/static/js/1.4e1b2920.chunk.js"
+  },
+  {
+    "revision": "f47ba623fa3d61a3fafb",
+    "url": "/weather-app/static/css/main.c5a46fb7.chunk.css"
+  },
+  {
+    "revision": "86ae120d435d6141dc1b66cf70298745",
+    "url": "/weather-app/index.html"
+  }
+];
